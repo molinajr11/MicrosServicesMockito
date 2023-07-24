9504d6c90038f9d24359abc6164a7d592d5f0000
Juan Esteban Molina Martinez <juanesteban.molinama@ibm.com>
2023-07-23 22:37:43 -0500

    first commit

---
+++ src/main/java/com/molina/demo/MicroServicesMokitoApplication.java
@@ -0,0 +1,13 @@
+package com.molina.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesMokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesMokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molina/demo/MicroServicesMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molina.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
